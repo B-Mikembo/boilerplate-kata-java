5bb369553afad0aec2a697c88be28376a276e4af
B-Mikembo <mikembo.brice.pro@gmail.com>
2026-09-25 15:45:00 +0200

    feat: should insert byte from ascii input at data pointer

--- src/test/java/com/brice/kata/BrainfuckTest.java
+++ src/test/java/com/brice/kata/BrainfuckTest.java
@@ -4,14 +4,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BrainfuckTest {
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   private final PrintStream originalOut = System.out;
+  private final InputStream originalIn = System.in;
+  private ByteArrayInputStream inContent;
 
   @BeforeEach
   void setUp() {
@@ -21,25 +23,28 @@ public class BrainfuckTest {
   @AfterEach
   void tearDown() {
     System.setOut(originalOut);
+    System.setIn(originalIn);
   }
 
   @Test
-  void shouldReturnEmptyArrayWhenMissingProgram() {
+  void shouldReturnEmptyArrayWhenMissingProgram() throws IOException {
     assertThat(execute("")).hasSize(30_000).containsOnly(0);
     assertThat(execute(null)).hasSize(30_000).containsOnly(0);
   }
 
-  private byte[] execute(String program) {
+  private byte[] execute(String program) throws IOException {
     if (program == null || program.isBlank())
       return new byte[30_000];
     byte[] bytes = new byte[30_000];
     int dataPointer = 0;
+    Scanner in = new Scanner(System.in);
     for (var character : program.toCharArray()) {
       switch (character) {
         case '+' -> bytes[dataPointer]++;
         case '-' -> bytes[dataPointer]--;
         case '>' -> dataPointer = (dataPointer + 1) % bytes.length;
         case '<' -> dataPointer = (dataPointer - 1) % bytes.length;
+        case ',' -> bytes[dataPointer] = (byte) System.in.read();
         default -> System.out.print((char) bytes[dataPointer]);
       }
     }
@@ -47,7 +52,7 @@ public class BrainfuckTest {
   }
 
   @Test
-  void shouldIncrement() {
+  void shouldIncrement() throws IOException {
     assertThat(execute("+")[0]).isEqualTo(toByte(1));
     assertThat(execute("++")[0]).isEqualTo(toByte(2));
   }
@@ -57,29 +62,37 @@ public class BrainfuckTest {
   }
 
   @Test
-  void shouldDecrement() {
+  void shouldDecrement() throws IOException {
     assertThat(execute("-")[0]).isEqualTo(toByte(255));
     assertThat(execute("--")[0]).isEqualTo(toByte(254));
   }
 
   //  [0,1,0,0,...]
   @Test
-  void shouldIncrementDataPointer() {
+  void shouldIncrementDataPointer() throws IOException {
     byte[] result = execute(">+");
     assertThat(result[0]).isEqualTo(toByte(0));
     assertThat(result[1]).isEqualTo(toByte(1));
   }
 
   @Test
-  void shouldDecrementDataPointer() {
+  void shouldDecrementDataPointer() throws IOException {
     byte[] result = execute("><+");
     assertThat(result[0]).isEqualTo(toByte(1));
     assertThat(result[1]).isEqualTo(toByte(0));
   }
 
   @Test
-  void shouldPrintAsciiValueAtDataPointer() {
+  void shouldPrintAsciiValueAtDataPointer() throws IOException {
     execute("+".repeat(72) + ".");
     assertThat(outContent.toString().trim()).isEqualTo("H");
+  }
+
+  @Test
+  void shouldInsertByteFromAsciiInputAtDataPointer() throws IOException {
+    inContent = new ByteArrayInputStream("H".getBytes());
+    System.setIn(inContent);
+    byte[] result = execute(",");
+    assertThat(result[0]).isEqualTo(toByte(72));
   }
 }
